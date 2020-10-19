42f35469307e24f2a38a052ad3ec825225b4d344
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-10-19 15:37:31 -0400

    scale deployment

---
+++ src/lib/k8s/scale.deployment.ts
@@ -0,0 +1,21 @@
+import k8s, { KubeConfig, V1Namespace } from '@kubernetes/client-node';
+
+interface Args {
+    namespace: string; name: string; replicas: number;
+};
+
+export async function scaleDeployment(kc: KubeConfig, args: Args) {
+
+    const { namespace, name, replicas} = args;
+    const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
+
+    // find the particular deployment
+    const res = await k8sApi.readNamespacedDeployment(name, namespace);
+    let deployment = res.body;
+  
+    // edit
+    deployment.spec.replicas = replicas;
+  
+    // replace
+    await k8sApi.replaceNamespacedDeployment(name, namespace, deployment);
+  }
