b4278528d2f698aabc8eb1053fd5971cb0fb836b
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-10-19 15:48:04 -0400

    loadKubernetesConfigurationFromDO

--- src/lib/k8s/config.kube.ts
+++ src/lib/k8s/config.kube.ts
@@ -1,12 +1,21 @@
-import * as k8s from '@kubernetes/client-node';
+import * as k8s from "@kubernetes/client-node";
+import { KubeConfig } from "@kubernetes/client-node";
+import DO from "do-wrapper";
+import { getK8sClusterConfig } from "../do";
 
 /**
- * Load kubernetes configurations from env cluster ID
+ * Load kubernetes configurations from digital ocean
+ * @param clusterId
+ * @param DO_KEY
  */
-export const loadKubernetesConfiguration = async (): Promise<KubeConfig> => {
-    const k8config = await getK8sClusterConfig(clusterId);
-    const kc = new k8s.KubeConfig();
-    kc.loadFromString(k8config);
-    return kc;
-}
+export const loadKubernetesConfigurationFromDO = async (
+  clusterId: string,
+  DO_KEY: string
+): Promise<KubeConfig> => {
+  const DigitalOcean = new DO(DO_KEY);
 
+  const k8config = await getK8sClusterConfig(clusterId, DigitalOcean);
+  const kc = new k8s.KubeConfig();
+  kc.loadFromString(k8config);
+  return kc;
+};
